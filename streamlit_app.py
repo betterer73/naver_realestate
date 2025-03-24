9caac30663e078e5df7d487fce358e9d9acbe282
betterer73 <mildsee@gmail.com>
2025-03-24 02:54:31 +0000

    Fix app loading issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.sidebar.title("메뉴")
 page = st.sidebar.radio("페이지 선택", ["네이버 아파트 분석", "카카오톡 대화 분석"])
 
 if page == "네이버 아파트 분석":
-    import naverapt_app  # 네이버 아파트 분석 페이지 로드
+    exec(open("naverapt_app.py", encoding="utf-8").read())  # 스크립트 실행
 elif page == "카카오톡 대화 분석":
-    import katalkranking  # 카카오톡 대화 분석 페이지 로드
+    exec(open("katalkranking.py", encoding="utf-8").read())  # 스크립트 실행
